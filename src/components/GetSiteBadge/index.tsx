3c31955c4d6a42624015a1ebd5299d3d21f672b8
Fábio Coelho <fabio_fc@hotmail.com>
2026-08-06 03:56:56 -0300

    Update index.tsx

--- src/components/GetSiteBadge/index.tsx
+++ src/components/GetSiteBadge/index.tsx
@@ -37,7 +37,7 @@ export const GetSiteBadge = () => {
         className="flex items-center gap-1.5 text-white no-underline hover:opacity-90"
       >
         <span id="getsite-badge-text" className="text-[12px] font-normal text-white/60 hover:text-white/90 transition-colors duration-200">Criado por</span>
-        <img src={logo} alt="GetSite" className="h-5 w-[55px] object-contain" />
+        <img src={logo} alt="GetSite" className="h-4 w-[48px] object-contain" />
       </a>
       <span id="getsite-badge-divider" aria-hidden="true" className="w-[1px] bg-white/5" />
       <button
